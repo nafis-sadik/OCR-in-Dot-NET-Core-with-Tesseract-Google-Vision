b0860025a4807cf0d0a6b39c894ba0e7f57ca7e9
Nafis Sadik <nafis_sadik@outlook.com>
2021-01-16 23:54:17 +0600

    Complete

---
+++ OCR-in-Dot-NET-Core-with-Tesseract/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace OCR_in_Dot_NET_Core_with_Tesseract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string imgPath = @"D:/Era Infotech Remote Job/OCR-in-Dot-NET-Core-with-Tesseract/OCR-in-Dot-NET-Core-with-Tesseract/TestImg.bmp";
+                Pix pix = Pix.LoadFromFile(imgPath);
+                TesseractEngine tesseractEngine = new TesseractEngine(@"C:/Program Files (x86)/Tesseract-OCR/tessdata", "eng", EngineMode.Default);
+                Page page = tesseractEngine.Process(pix, PageSegMode.Auto);
+                string result = page.GetText();
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
