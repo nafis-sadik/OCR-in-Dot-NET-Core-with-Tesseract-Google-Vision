87903b48f39dceee07b5252774d87e992edfc39f
Nafis Sadik <nafis_sadik@outlook.com>
2021-01-24 13:27:48 +0600

    OCR works

--- OCR using Google Cloud Vision API/Program.cs
+++ OCR using Google Cloud Vision API/Program.cs
@@ -7,23 +7,7 @@ using Google.Apis.Auth.OAuth2;
 namespace OCR_using_Google_Cloud_Vision_API
 {
     class Program
-    {
-        // Some APIs, like Storage, accept a credential in their Create()
-        // method.
-        public void AuthExplicit(string projectId, string jsonPath)
-        {
-            // Explicitly use service account credentials by specifying 
-            // the private key file.
-            var credential = GoogleCredential.FromFile(jsonPath);
-            var storage = StorageClient.Create(credential);
-            // Make an authenticated API request.
-            var buckets = storage.ListBuckets(projectId);
-            foreach (var bucket in buckets)
-            {
-                Console.WriteLine(bucket.Name);
-            }
-        }
-
+    { 
         static void LoadImg (string imgPath)
         {
             Image image = Image.FromFile(imgPath);
